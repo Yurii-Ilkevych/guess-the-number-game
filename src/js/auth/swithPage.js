07aa3b394dcec584e5c0de17aca57d3d580c13df
Yurii-Ilkevych <rufiktcl@gmail.com>
2024-10-21 09:11:19 +0300

    Update swithPage.js

--- src/js/auth/swithPage.js
+++ src/js/auth/swithPage.js
@@ -1,11 +1,12 @@
 import { getUser } from "../working-with-memory"
+const baseUrl = document.querySelector("base").baseURI
 
 export function switchPage(){
-
+  console.log()
 if(getUser() === null && window.location.pathname.includes("game.html")){
-  
-window.location.href = "/"
+ 
+window.location.href = baseUrl
 }else if(getUser() !== null && !window.location.pathname.includes("game.html")){
-window.location.href = "/game.html"
+window.location.href = baseUrl + "game.html"
 }
 }
